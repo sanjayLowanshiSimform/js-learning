a59d3b8515768d6818c60e2039b30e55ff62770e
Sanjay Lowanshi <sanjay.l@simformsolutions.com>
2024-01-12 20:41:26 +0530

    calorie counter 57: done

--- FREECODECAMP/Calorie-Counter/script.js
+++ FREECODECAMP/Calorie-Counter/script.js
@@ -12,5 +12,28 @@ function cleanInputString(str) {
 }
 
 function isInvalidInput(str) {
-  const regex = /[0-9]+e[0-9]+/i;
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
 }
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+function getCaloriesFromInputs(list) {
+
+}
+
+addEntryButton.addEventListener("click", addEntry);
